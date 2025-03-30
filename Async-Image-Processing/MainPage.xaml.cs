5f6df1339c090a886d2fdf980809a45e1a33cb3d
Sebastian Hollersbacher <s.hollersbacher@outlook.at>
2025-03-30 17:38:45 +0200

    Added loading of images

--- Async-Image-Processing/MainPage.xaml.cs
+++ Async-Image-Processing/MainPage.xaml.cs
@@ -1,25 +1,66 @@
-﻿namespace Async_Image_Processing
+﻿using System.Collections.ObjectModel;
+
+namespace Async_Image_Processing
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage
     {
-        int count = 0;
+        public ObservableCollection<ImageSource> ImagesList { get; set; } = new();
+        private string _imagesDirectory = ""; // TODO: make selection of Path 
+        private CancellationTokenSource _cts;
 
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void OnCancelLoadingClicked(object sender, EventArgs e)
         {
-            count++;
+            _cts?.Cancel();
+            ImageProgressBar.Progress = 0;
+        }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+        private async void OnLoadImagesClicked(object sender, EventArgs e)
+        {
+            _cts?.CancelAsync();
+            _cts = new CancellationTokenSource();
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            try
+            {
+                await LoadImagesAsync(_cts.Token);
+            }
+            catch (OperationCanceledException)
+            {
+                // TODO: handle cancel
+            }
+        }
+
+        private async Task LoadImagesAsync(CancellationToken cancellationToken)
+        {
+            // Worker Thread
+            ImagesList.Clear();
+            int loadedImages = 0;
+            string[] imageFiles = Directory.GetFiles(_imagesDirectory, "*.jpg");
+
+            IProgress<ImageSource> progress = new Progress<ImageSource>(image =>
+            {
+                // on UI thread
+                ImagesList.Add(image);
+                ImageProgressBar.Progress = (double)++loadedImages / imageFiles.Length;
+            });
+
+            // Worker Threads
+            await Parallel.ForEachAsync(imageFiles, cancellationToken, async (file, token) =>
+            {
+                // Worker Thread
+                var imageSource = ImageSource.FromFile(file);
+                progress.Report(imageSource);
+
+                await Task.Delay(100, token); // Delay to not overwhelm UI
+            });
+            
+            // On UI Thread
+            await MainThread.InvokeOnMainThreadAsync(() => ImageProgressBar.Progress = 1);
         }
     }
-
 }
